5ce5ff914454119763aff06ab35108249e6100f7
gaparmar <gparmar@cs.cmu.edu>
2024-03-19 10:12:42 -0400

    adding some instructions

--- gradio_sketch2image.py
+++ gradio_sketch2image.py
@@ -297,6 +297,12 @@ with gr.Blocks(css="style.css") as demo:
                 show_download_button=True,
             )
             download_output = gr.Button("Download output", elem_id="download_output")
+            gr.Markdown("### Instructions")
+            gr.Markdown("**1**. Enter a text prompt.")
+            gr.Markdown("**2**. Sketch the image yuo want to draw")
+            gr.Markdown("**3**. Select the style of the image with the Prompt Style Template")
+            gr.Markdown("**4**. Adjust the Sketch guidance gamma to control the influence of the sketch on the final image")
+            gr.Markdown("**5**. Try different seeds to get different results")
 
     eraser.change(
         fn=lambda x: gr.update(value=not x),
